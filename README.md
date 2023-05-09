eab276395d48510feae057759c859a9d20b04b3e
NamitBhutani <namit.bhut@gmail.com>
2023-05-09 12:47:16 +0530

    added payto svelte component and new logo

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ This is a task project for the inductions to SWD Nucleus Tech Team in BITS Pilan
 The task is to a create a vendor/user payment system where user scans a qr code and transfers money to Vendor. User and Vendor signup/login system also needs to be implemented.
 
 (Made using Sveltekit, Supabase)
+To do-
+
+1. Complete the /payto/[id] route
+2. RLS in supabase for restricting access to tables
+3. Make a separate vendorsProfiles(ones who have confirmed their mail)table and pass data from auth.Users table to it on entry
+4. Protect routes in hook.server.ts
+5. Fix build errors
--- src/routes/payto/[id]/+page.svelte
+++ src/routes/payto/[id]/+page.svelte
@@ -5,3 +5,32 @@
 </script> -->
 
 <!-- <p>{vendorBalance}</p> -->
+<form action="" method="POST">
+	<div class="grid">
+		<div>
+			<label class="payto" for="payto">Paying To:</label>
+			<label class="amount" for="amount">Enter the amount you want to pay-</label>
+		</div>
+		<div class="input">
+			<input type="text" name="amount" id="amount" required />
+		</div>
+	</div>
+	<button type="submit">Pay</button>
+</form>
+
+<style>
+	.payto,
+	.amount {
+		text-align: center;
+		margin-bottom: 0.5rem;
+	}
+	form {
+		width: 95%;
+		margin-left: 2.5%;
+		margin-top: 1rem;
+	}
+	.input {
+		margin: auto;
+		width: 100%;
+	}
+</style>
